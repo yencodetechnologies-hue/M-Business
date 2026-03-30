4bb36b9934c95539553a598851263a53ef963668
Irin-07 <irinamalfelshiyaa@gmail.com>
2026-03-30 13:00:42 +0530

    T

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ const uploadRoutes        = require("./routes/uploadroutes");
 const employeeDashRoutes  = require("./routes/employeedashboardroutes");
 const QuotationRoutes     = require("./routes/quotationroutes");
 const interviewRoutes     = require("./routes/interviewroutes");
-
+const userRoutes = require('./routes/userroutes');
 // Static files (local resume storage)
 const path = require("path");
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));

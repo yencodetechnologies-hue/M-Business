cde6f83373bd1a67bafdd20ec48c5fb0a6609e31
Irin-07 <irinamalfelshiyaa@gmail.com>
2026-04-27 17:20:06 +0530

    T

--- M Business/src/config.js
+++ M Business/src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
   ? "http://localhost:5000"
-  : "https://m-business-r2vd.onrender.com";
+  : "https://mbusiness.octosofttechnologies.in";
 
 // Global interceptor for Multi-Tenant Architecture
 axios.interceptors.request.use((config) => {

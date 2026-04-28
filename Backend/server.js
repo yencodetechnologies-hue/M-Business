0b2638ec817fa84a1ffdeaecaa94abbbd777088f
Irin-07 <irinamalfelshiyaa@gmail.com>
2026-04-28 15:36:17 +0530

    T

--- Backend/server.js
+++ Backend/server.js
@@ -13,24 +13,25 @@ const isLocalhost = (origin) =>
     
   );
 
+const allowedOrigins = [
+  "https://m-business-tau.vercel.app",
+  "https://mbusiness.octosofttechnologies.in",
+  "http://localhost:5173",
+  "http://localhost:3000"
+];
+
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
-
-    if (isLocalhost(origin)) return callback(null, true);
-
-    const allowedOrigins = [
-      "https://m-business-tau.vercel.app",
-      "https://mbusiness.octosofttechnologies.in",
-   ];
-
-    if (allowedOrigins.includes(origin)) {
+    if (isLocalhost(origin) || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-company-id", "Accept"]
 }));
 app.use(express.json());
 

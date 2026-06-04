6909a95c25521ac307e7fd08c72652af13551d5f
Irin-07 <irinamalfelshiyaa@gmail.com>
2026-06-04 11:29:55 +0530

    login

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://www.mbusiness.cloud",
-  "https://mbusiness.cloud/"
+  "https://mbusiness.cloud"
 ];
 
 app.use(cors({

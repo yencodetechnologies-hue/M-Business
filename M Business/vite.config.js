e46a7151913e33d0d553027db2cd2649d364a4bf
Irin-07 <irinamalfelshiyaa@gmail.com>
2026-06-11 15:14:37 +0530

    employees

--- M Business/vite.config.js
+++ M Business/vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
+  build: {
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          axios: ['axios'],
+        }
+      }
+    }
   }
 })

1f35c21213a359f5433cadfddf70c249b59c5ade
Irin-07 <irinamalfelshiyaa@gmail.com>
2026-07-28 10:03:16 +0530

    ui

--- M Business/src/components/ClientDashboard.jsx
+++ M Business/src/components/ClientDashboard.jsx
@@ -3572,8 +3572,6 @@ export default function ClientDashboard({ user: userProp, setUser, portalMode = 
               <div style={{ flex: 1, background: "#f4f4f4", display: "flex", alignItems: "center", justifyContent: "center", overflow: "auto" }}>
                 {isPreviewableFile(previewFile) === "image" ? (
                   <img src={previewFile.url} alt={previewFile.name} style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "contain" }} />
-                ) : isPreviewableFile(previewFile) === "office" ? (
-                  <iframe src={`https://docs.google.com/gview?url=${encodeURIComponent(previewFile.url)}&embedded=true`} title={previewFile.name} style={{ width: "100%", height: "100%", border: "none" }} />
                 ) : (
                   <iframe src={previewFile.url} title={previewFile.name} style={{ width: "100%", height: "100%", border: "none" }} />
                 )}
